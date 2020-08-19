864aa57dad83de79254fdfa2c817d912d2306990
7Romuch7 <64313145+7Romuch7@users.noreply.github.com>
2020-08-19 22:08:06 +0300

    Решения задач

---
+++ js/task-06.js
@@ -0,0 +1,7 @@
+/*let total = 0;
+let input = +prompt(`Введите произвольное число`);
+while(input) {
+    total += input;
+    input = +prompt();
+}
+alert("Общая сумма чисел равна [" + total +"]");*/
